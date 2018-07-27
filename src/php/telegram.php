24583877d5aff602e5ced35ef74a0c57b79b19be
KEZOV <kep45567@mail.ru>
2018-07-27 11:30:30 +0300

    Delete telegram.php

--- src/php/telegram.php
+++
@@ -1,9 +0,0 @@
-<?php
-$apiToken = "513832109:AAHtALyxW2UQqLGGw4cs2LYhn8vKIAlsVng";
-
-$data = [
-  'chat_id' => '@SkanWIFI_bot',
-  'text' => 'Эта херня пришла'
-];
-
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
